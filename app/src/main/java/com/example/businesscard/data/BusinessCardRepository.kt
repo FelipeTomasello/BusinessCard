8d529360368e8b31209b499848e3b7409adb27d5
Felipe Tomasello <felipe.tomasello@gmail.com>
2022-08-11 17:28:39 -0300

    Adiconando Room a aplicação

---
+++ app/src/main/java/com/example/businesscard/data/BusinessCardRepository.kt
@@ -0,0 +1,18 @@
+package com.example.businesscard.data
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
+
+class BusinessCardRepository(
+    private val dao: BusinessCardDao
+) {
+    fun insert(businessCard: BusinessCard) = runBlocking {
+        launch (Dispatchers.IO) {
+            dao.insert(businessCard)
+        }
+    }
+
+    fun getAll() = dao.getAll()
+
+}
